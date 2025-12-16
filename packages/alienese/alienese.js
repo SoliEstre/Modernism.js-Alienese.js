32bf445c5b60a321c32a61691ba9e9b87d4337a7
SoliEstre <estresoliette@gmail.com>
2025-12-16 18:27:05 +0900

    feat: Add alienese library for JavaScript aliasing and shorthand constants.

--- packages/alienese/alienese.js
+++ packages/alienese/alienese.js
@@ -32,7 +32,7 @@ SOFTWARE.
 // This patch aims to create smaller (quicker) and more concise (lighter) JavaScript code.
 // It makes the code more implicit and serves as an alternative to obfuscation.
 // 
-// v0.5.0 / release 2025.12.09
+// v0.5.1 / release 2025.12.16
 // 
 // * Must be loaded modernism.js before this script.
 // 
@@ -55,6 +55,139 @@ let dfg = defineGlobal ?? ((name, value) => {
         enumerable: false
     });
 });
+
+
+// common letter constants
+dfg("_0", "0");
+dfg("_1", "1");
+dfg("_2", "2");
+dfg("_3", "3");
+dfg("_4", "4");
+dfg("_5", "5");
+dfg("_6", "6");
+dfg("_7", "7");
+dfg("_8", "8");
+dfg("_9", "9");
+
+dfg("_a", "a");
+dfg("_b", "b");
+dfg("_c", "c");
+dfg("_d", "d");
+dfg("_e", "e");
+dfg("_f", "f");
+dfg("_g", "g");
+dfg("_h", "h");
+dfg("_i", "i");
+dfg("_j", "j");
+dfg("_k", "k");
+dfg("_l", "l");
+dfg("_m", "m");
+dfg("_n", "n");
+dfg("_o", "o");
+dfg("_p", "p");
+dfg("_q", "q");
+dfg("_r", "r");
+dfg("_s", "s");
+dfg("_t", "t");
+dfg("_u", "u");
+dfg("_v", "v");
+dfg("_w", "w");
+dfg("_x", "x");
+dfg("_y", "y");
+dfg("_z", "z");
+
+dfg("_A", "A");
+dfg("_B", "B");
+dfg("_C", "C");
+dfg("_D", "D");
+dfg("_E", "E");
+dfg("_F", "F");
+dfg("_G", "G");
+dfg("_H", "H");
+dfg("_I", "I");
+dfg("_J", "J");
+dfg("_K", "K");
+dfg("_L", "L");
+dfg("_M", "M");
+dfg("_N", "N");
+dfg("_O", "O");
+dfg("_P", "P");
+dfg("_Q", "Q");
+dfg("_R", "R");
+dfg("_S", "S");
+dfg("_T", "T");
+dfg("_U", "U");
+dfg("_V", "V");
+dfg("_W", "W");
+dfg("_X", "X");
+dfg("_Y", "Y");
+dfg("_Z", "Z");
+
+
+// common extra characters constants
+dfg("lr", "(");
+dfg("rr", ")");
+dfg("lc", "{");
+dfg("rc", "}");
+dfg("ls", "[");
+dfg("rs", "]");
+dfg("lt", "<");
+dfg("gt", ">");
+dfg("ab", lt + gt);
+dfg("cb", gt + lt);
+dfg("ti", "~");
+dfg("ep", "!");
+dfg("em", ep);
+dfg("at", "@");
+dfg("ds", "$");
+dfg("ms", "&");
+dfg("ps", "%");
+dfg("cf", "^");
+dfg("ak", "*");
+dfg("mp", ak);
+dfg("ad", "+");
+dfg("add", ad + ad);
+dfg("hp", "-");
+dfg("sr", hp);
+dfg("srr", sr + sr);
+dfg("us", "_");
+dfg("eq", "=");
+dfg("vl", "|");
+dfg("bs", "\\");
+dfg("ss", "/");
+dfg("dv", ss);
+dfg("qm", "?");
+dfg("nl", ep + eq);
+dfg("le", lt + eq);
+dfg("ge", gt + eq);
+dfg("fa", ad + eq);
+dfg("fs", sr + eq);
+dfg("fm", mp + eq);
+dfg("fd", dv + eq);
+dfg("sq", "'");
+dfg("dq", '"');
+dfg("gv", '`');
+dfg("cl", ":");
+dfg("sc", ";");
+dfg("cm", ",");
+dfg("es", "");
+dfg("l", cm);
+dfg("s", " ");
+dfg("i", "#");
+dfg("d", ".");
+
+dfg("cr", "\r");
+dfg("lf", "\n");
+dfg("crlf", cr + lf);
+dfg("lfcr", lf + cr);
+dfg("tab", "\t");
+
+dfg("ecr", "\\r");
+dfg("elf", "\\n");
+dfg("ecrlf", ecr + elf);
+dfg("elfcr", elf + ecr);
+dfg("etab", "\\t");
+
 
 // primitive types alias constant
 dfg("U", UNDEFINED);
@@ -308,6 +441,7 @@ dfg("dpgsx", _gb.defineGetterAndSetterPlex);
 // additional static function for classes
 defineStaticGetterAndSetter(dt, "n", function () { return new Date(...arguments); });
 defineStaticGetterAndSetter(dt, "t", function () { return this.now(); });
+defineStaticGetterAndSetter(dt, "ttt", function () { return (this.now() + "").padStart(3, "0"); });
 
 
 // additional global prototype functions
@@ -324,37 +458,118 @@ definePropertyPlex("ne", function (process = it => it, ornot = it => it, numberE
 definePropertyPlex("dr", function (does = (it, args) => {}, returns, args = []) { return _gb.doAndReturn(does, returns, [this.it, ...args]); });
 definePropertyPlex("drx", function (does = (it, args) => {}, forReturns, args = []) { return _gb.doAndReturnByExecute(does, forReturns, [this.it, ...args]); });
 
-defineGetterAndSetter(Object, "ok", function () { return _gb.keysOf(this.it); });
-defineGetterAndSetter(Object, "ov", function () { return _gb.valuesOf(this.it); });
-defineGetterAndSetter(Object, "oe", function () { return _gb.entriesOf(this.it); });
-defineGetterAndSetter(Object, "oc", function () { return _gb.countOf(this.it); });
-defineGetterAndSetter(Object, "occ", function () { return _gb.checkCount(this.it, (k, v) => true); });
+defineGetterAndSetter(obj, "ok", function () { return _gb.keysOf(this.it); });
+defineGetterAndSetter(obj, "ov", function () { return _gb.valuesOf(this.it); });
+defineGetterAndSetter(obj, "oe", function () { return _gb.entriesOf(this.it); });
+defineGetterAndSetter(obj, "oc", function () { return _gb.countOf(this.it); });
+defineGetterAndSetter(obj, "occ", function () { return _gb.checkCount(this.it, (k, v) => true); });
 
-defineProperty(Object, "fk", function (work = key => { return false; }) { return _gb.forof(this.it.ways, work); });
-defineProperty(Object, "fv", function (work = value => { return false; }) { return _gb.forof(this.it.looks, work); });
-defineProperty(Object, "fe", function (work = (key, value) => { return false; }) { return _gb.forkv(this.it.entire, work); });
-defineProperty(Object, "fkv", function (work = (key, value) => { return false; }) { return _gb.forkv(this.it.entire, work); });
+defineProperty(obj, "fk", function (work = key => { return false; }) { return _gb.forof(this.it.ways, work); });
+defineProperty(obj, "fv", function (work = value => { return false; }) { return _gb.forof(this.it.looks, work); });
+defineProperty(obj, "fe", function (work = (key, value) => { return false; }) { return _gb.forkv(this.it.entire, work); });
+defineProperty(obj, "fkv", function (work = (key, value) => { return false; }) { return _gb.forkv(this.it.entire, work); });
 
-defineProperty(Object, "ko", function (value) { for (const [key, val] of this.it.entire) if (val === value) return key; return undefined; });
+defineProperty(obj, "ko", function (value) { for (const [key, val] of this.it.entire) if (val === value) return key; return undefined; });
 
-defineProperty(Object, "tiw", function (key, process = (value, key, host) => value, ornot = (key, host) => host[key]) { return key in this.it ? process(this.it[key], key, this.it) : ornot(key, this.it); });
+defineProperty(obj, "tiw", function (key, process = (value, key, host) => value, ornot = (key, host) => host[key]) { return key in this.it ? process(this.it[key], key, this.it) : ornot(key, this.it); });
 
-defineProperty(Object, "cp", function (dataOnly = true, primitiveOnly = false, recusive = true) { return _gb.copy(this, dataOnly, primitiveOnly, recusive); });
-defineGetterAndSetter(Object, "mk", function () { return _gb.mock(this); });
-defineGetterAndSetter(Object, "mm", function () { return _gb.mimic(this); });
-defineGetterAndSetter(Object, "tw", function () { return _gb.twin(this); });
-defineGetterAndSetter(Object, "cl", function () { return _gb.clone(this); });
+defineProperty(obj, "cp", function (dataOnly = true, primitiveOnly = false, recusive = true) { return _gb.copy(this, dataOnly, primitiveOnly, recusive); });
+defineGetterAndSetter(obj, "mk", function () { return _gb.mock(this); });
+defineGetterAndSetter(obj, "mm", function () { return _gb.mimic(this); });
+defineGetterAndSetter(obj, "tw", function () { return _gb.twin(this); });
+defineGetterAndSetter(obj, "cl", function () { return _gb.clone(this); });
 
-defineProperty(Object, "pc", function (from, dataOnly = true, primitiveOnly = false, recusive = true, append = false) { return _gb.patch(this.it, from, dataOnly, primitiveOnly, recusive, append); });
-defineProperty(Object, "ow", function (from) { return _gb.overwrite(this.it, from); });
-defineProperty(Object, "to", function (from) { return _gb.takeover(this.it, from); });
-defineProperty(Object, "aq", function (from) { return _gb.acquire(this.it, from); });
-defineProperty(Object, "ih", function (from) { return _gb.inherit(this.it, from); });
+defineProperty(obj, "pc", function (from, dataOnly = true, primitiveOnly = false, recusive = true, append = false) { return _gb.patch(this.it, from, dataOnly, primitiveOnly, recusive, append); });
+defineProperty(obj, "ow", function (from) { return _gb.overwrite(this.it, from); });
+defineProperty(obj, "to", function (from) { return _gb.takeover(this.it, from); });
+defineProperty(obj, "aq", function (from) { return _gb.acquire(this.it, from); });
+defineProperty(obj, "ih", function (from) { return _gb.inherit(this.it, from); });
 
-defineProperty(Object, "rv", function (from, dataOnly = true, primitiveOnly = false, recusive = true, exceptNew = false) { return _gb.revert(this.it, from, dataOnly, primitiveOnly, recusive, exceptNew); });
+defineProperty(obj, "rv", function (from, dataOnly = true, primitiveOnly = false, recusive = true, exceptNew = false) { return _gb.revert(this.it, from, dataOnly, primitiveOnly, recusive, exceptNew); });
 
 definePropertyPlex("ifeq", function (that, process = it => it, ornot = it => {}) { return this.let(it => _gb.executeIf(it == that, process, [it], ornot)); });
 definePropertyPlex("ifneq", function (that, process = it => it, ornot = it => {}) { return this.let(it => _gb.executeIf(it != that, process, [it], ornot)); });
+
+
+// additional primitive prototype functions
+defineGetterAndSetter(num, "abs", function () { return Math.abs(this.it); });
+defineGetterAndSetter(num, "int", function () { return parseInt(this.it); });
+defineGetterAndSetter(num, "prc", function () { return this.it - this.int; });
+defineGetterAndSetter(num, "prs", function () { return (this.prc + "").replace(/^0\./, ""); });
+defineGetterAndSetter(num, "pri", function () { return parseInt(this.prs); });
+defineGetterAndSetter(num, "d00", function () { return this.it.let(it => ((it < 0 ? hp : "") + it.abs).padStart(2, _0)); });
+defineGetterAndSetter(num, "d000", function () { return this.it.let(it => ((it < 0 ? hp : "") + it.abs).padStart(3, _0)); });
+defineGetterAndSetter(num, "d0000", function () { return this.it.let(it => ((it < 0 ? hp : "") + it.abs).padStart(4, _0)); });
+
+defineGetterAndSetter(str, "low", function () { return this.it.toLowerCase(); });
+defineGetterAndSetter(str, "upp", function () { return this.it.toUpperCase(); });
+defineGetterAndSetter(str, "cap", function () { return this.it.let(it => it[0].upp + it.substring(1)); });
+defineGetterAndSetter(str, "int", function () { return parseInt(this.it); });
+defineGetterAndSetter(str, "float", function () { return parseFloat(this.it); });
+defineGetterAndSetter(str, "d00", function () { return this.it.padStart(2, _0); });
+defineGetterAndSetter(str, "d000", function () { return this.it.padStart(3, _0); });
+defineGetterAndSetter(str, "d0000", function () { return this.it.padStart(4, _0); });
+
+defineGetterAndSetter(dt, "year", function () { return this.getFullYear(); });
+defineGetterAndSetter(dt, "yyyy", function () { return this.year; });
+defineGetterAndSetter(dt, "y", function () { return this.yyyy; });
+defineGetterAndSetter(dt, "yy", function () { return (this.yyyy % 100).d00; });
+defineGetterAndSetter(dt, "M0", function () { return this.getMonth(); });
+defineGetterAndSetter(dt, "M", function () { return this.M0 + 1; });
+defineGetterAndSetter(dt, "MM", function () { return this.M.d00; });
+defineGetterAndSetter(dt, "d", function () { return this.getDate(); });
+defineGetterAndSetter(dt, "dd", function () { return this.d.d00; });
+defineGetterAndSetter(dt, "U", function () { return this.getDay(); });
+defineGetterAndSetter(dt, "u", function () { return this.U.let(it => it == 0 ? 7 : it); });
+defineGetterAndSetter(dt, "uu", function () { return ["su", "mo", "tu", "we", "th", "fr", "sa"][this.U]; });
+defineGetterAndSetter(dt, "Uu", function () { return this.uu.cap; });
+defineGetterAndSetter(dt, "UU", function () { return this.uu.upp; });
+defineGetterAndSetter(dt, "uuu", function () { return ["sun", "mon", "tue", "wed", "thu", "fri", "sat"][this.U]; });
+defineGetterAndSetter(dt, "Uuu", function () { return this.uuu.cap; });
+defineGetterAndSetter(dt, "UUU", function () { return this.uuu.upp; });
+defineGetterAndSetter(dt, "uuuu", function () { return ["sun", "mon", "tues", "wedns", "thurs", "fri", "satur"][this.U]; });
+defineGetterAndSetter(dt, "Uuuu", function () { return this.uuuu.cap; });
+defineGetterAndSetter(dt, "UUUU", function () { return this.uuuu.upp; });
+defineGetterAndSetter(dt, "day", function () { return this.uuuu + "day"; });
+defineGetterAndSetter(dt, "Day", function () { return this.day.cap; });
+defineGetterAndSetter(dt, "DAY", function () { return this.day.upp; });
+defineGetterAndSetter(dt, "yoil", function () { return ["일", "월", "화", "수", "목", "금", "토"][this.U]; });
+defineGetterAndSetter(dt, "YOIL", function () { return this.yoil + "요일"; });
+defineGetterAndSetter(dt, "youbi", function () { return ["日", "月", "火", "水", "木", "金", "土"][this.U]; });
+defineGetterAndSetter(dt, "YOUBI", function () { return this.youbi + "曜日"; });
+defineGetterAndSetter(dt, "H", function () { return this.getHour(); });
+defineGetterAndSetter(dt, "HH", function () { return this.H.d00; });
+defineGetterAndSetter(dt, "a", function () { return this.H < 12 ? _A : _P; });
+defineGetterAndSetter(dt, "aa", function () { return this.a + _M; });
+defineGetterAndSetter(dt, "h", function () { return (this.H % 12).let(it => it == 0 ? 12 : it); });
+defineGetterAndSetter(dt, "hh", function () { return this.h.d00; });
+defineGetterAndSetter(dt, "m", function () { return this.getMinutes(); });
+defineGetterAndSetter(dt, "mm", function () { return this.m.d00; });
+defineGetterAndSetter(dt, "s", function () { return this.getSeconds(); });
+defineGetterAndSetter(dt, "ss", function () { return this.s.d00; });
+defineGetterAndSetter(dt, "S", function () { return this.getMilliseconds(); });
+defineGetterAndSetter(dt, "SSS", function () { return this.S.d000; });
+defineGetterAndSetter(dt, "O", function () { return this.getTimezoneOffset(); });
+defineGetterAndSetter(dt, "Z", function () { return this.O / 60; });
+defineGetterAndSetter(dt, "Zm", function () { return this.O % 60; });
+defineGetterAndSetter(dt, "Zmm", function () { return this.Zm.d00; });
+defineGetterAndSetter(dt, "Zh", function () { return this.Z.int; });
+defineGetterAndSetter(dt, "Zhh", function () { return this.Zh.d00; });
+defineGetterAndSetter(dt, "X", function () { return this.Zhh + cl + this.Zmm; });
+defineGetterAndSetter(dt, "time", function () { return this.getTime(); });
+defineGetterAndSetter(dt, "unix", function () { return (this.t / 1000).int; });
+defineGetterAndSetter(dt, "t", function () { return this.time; });
+defineGetterAndSetter(dt, "ut", function () { return this.unix; });
+defineGetterAndSetter(dt, "mo", function () { return this.y * 12 + this.M0; });
+defineGetterAndSetter(dt, "do", function () { return (((this.time / 60 / 60 / 1000) + (this.O / -60)) / 24).int; });
+defineGetterAndSetter(dt, "yearMonth", function () { return this.YYYY + hp + this.MM; });
+defineGetterAndSetter(dt, "ym", function () { return this.yearMonth; });
+defineGetterAndSetter(dt, "dateString", function () { return this.YYYY + hp + this.MM + hp + this.dd; });
+defineGetterAndSetter(dt, "ymd", function () { return this.dateString; });
+defineGetterAndSetter(dt, "hourMinutes", function () { return this.HH + cl + this.mm; });
+defineGetterAndSetter(dt, "hm", function () { return this.hourMinutes; });
+defineGetterAndSetter(dt, "timeString", function () { return this.HH + cl + this.mm + cl + this.ss; });
+defineGetterAndSetter(dt, "hms", function () { return this.timeString; });
 
 
 
@@ -368,70 +583,3 @@ dfg("rig", regex => new RegExp(regex, "ig"));
 dfg("rim", regex => new RegExp(regex, "im"));
 dfg("rgm", regex => new RegExp(regex, "gm"));
 dfg("rigm", regex => new RegExp(regex, "igm"));
-
-
-
-// common extra characters constants
-dfg("lr", "(");
-dfg("rr", ")");
-dfg("lc", "{");
-dfg("rc", "}");
-dfg("ls", "[");
-dfg("rs", "]");
-dfg("lt", "<");
-dfg("gt", ">");
-dfg("ab", _gb.lt + _gb.gt);
-dfg("cb", _gb.gt + _gb.lt);
-dfg("ti", "~");
-dfg("ep", "!");
-dfg("em", _gb.ep);
-dfg("at", "@");
-dfg("ds", "$");
-dfg("ms", "&");
-dfg("ps", "%");
-dfg("cf", "^");
-dfg("ak", "*");
-dfg("mp", _gb.ak);
-dfg("ad", "+");
-dfg("add", _gb.ad + _gb.ad);
-dfg("hp", "-");
-dfg("sr", _gb.hp);
-dfg("srr", _gb.sr + _gb.sr);
-dfg("us", "_");
-dfg("eq", "=");
-dfg("vl", "|");
-dfg("bs", "\\");
-dfg("ss", "/");
-dfg("dv", _gb.ss);
-dfg("qm", "?");
-dfg("nl", _gb.ep + _gb.eq);
-dfg("le", _gb.lt + _gb.eq);
-dfg("ge", _gb.gt + _gb.eq);
-dfg("fa", _gb.ad + _gb.eq);
-dfg("fs", _gb.sr + _gb.eq);
-dfg("fm", _gb.mp + _gb.eq);
-dfg("fd", _gb.dv + _gb.eq);
-dfg("sq", "'");
-dfg("dq", '"');
-dfg("gv", '`');
-dfg("cl", ":");
-dfg("sc", ";");
-dfg("cm", ",");
-dfg("es", "");
-dfg("l", _gb.cm);
-dfg("s", " ");
-dfg("i", "#");
-dfg("d", ".");
-
-dfg("cr", "\r");
-dfg("lf", "\n");
-dfg("crlf", _gb.cr + _gb.lf);
-dfg("lfcr", _gb.lf + _gb.cr);
-dfg("tab", "\t");
-
-dfg("ecr", "\\r");
-dfg("elf", "\\n");
-dfg("ecrlf", _gb.ecr + _gb.elf);
-dfg("elfcr", _gb.elf + _gb.ecr);
-dfg("etab", "\\t");
-
